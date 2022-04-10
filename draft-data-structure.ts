e9c53941ea8cd893d77c0f39416bdaf84fd4785a
Rafael Silveira <oi@rafasilveira.dev>
2022-04-10 09:16:05 -0300

    feature(*): monorepo structure

---
+++ draft-data-structure.ts
@@ -0,0 +1,50 @@
+/**
+ * o usuário pode pertencer a várias equipes
+ * uma equipe pode ter vários usuários (agência, setor de mkt, ...)
+ * uma equipe pode ter vários projetos (site, blog, campanha X, Y, Z, ...)
+ * cada projeto pode ter vários formulários
+ *
+ * Fase 1: submissão de forms (FormItem, FormSubmission, Form)
+ * Fase 2: controle de acesso (Project, Team, User)
+ */
+
+interface FormItem {
+  key: string
+  value: string
+}
+
+interface FormSubmission {
+  data: FormItem[]
+  createdAt: string
+}
+
+interface Form {
+  id: string
+  teamId: string
+  name: string
+  createdAt: string
+  updatedAt: string
+  submissions: FormSubmission[]
+}
+
+interface Project {
+  id: string
+  teamId: string
+  forms: string[]
+}
+
+interface Team {
+  id: string
+  projects: string[]
+  users: string[]
+  name: string
+}
+
+interface User {
+  id: string
+  teams: string[]
+  role: string
+  name: string
+  email: string
+  password: string
+}
